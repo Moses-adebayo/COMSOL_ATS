73b477d70ed35cb055de4c8312e210dc61e282a0
Moses Adebayo (Student) <moses_adebayo@mines.edu>
2025-08-06 19:04:52 -0600

    starting to invert shourt model hourly using ESMDA

---
+++ param8/model.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -A CLI185
+#SBATCH -N 1
+#SBATCH -J test
+#SBATCH --mail-user=moses_adebayo@mines.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:45:00
+#SBATCH -p batch
+srun -n 80 ats --xml_file=../BSL_profile_convert8.xml &> out.log
