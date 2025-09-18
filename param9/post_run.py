70f9b8b46d51df3f64e2a3d071d0cff3ab97847e
Moses Adebayo <mosesadebayo001@baseline-login2.ccs.ornl.gov>
2025-09-17 22:20:36 -0400

    house keeping for updated source code

--- param9/post_run.py
+++ param9/post_run.py
@@ -8,7 +8,9 @@ import os
 import time
 #from write_script import write_script
 import zipfile
-param=np.loadtxt('../param9.txt') 
+import sys
+i = int(sys.argv[1])
+param=np.loadtxt('../param'+str(i)+'.txt') 
 file_path = 'ats_vis_data.VisIt.xmf'
 
 # Read the file content
@@ -27,9 +29,8 @@ for i in range(len(numbers)):
     temp=loc.copy()
     df=h5py.File('ats_vis_data.h5', 'r')
     fin['porosity']=df['base_porosity'][str(numbers[i])]
-    fin['U']=df['darcy_velocity.0'][str(numbers[i])]
-    fin['W']=df['darcy_velocity.2'][str(numbers[i])]
-    fin['k']=df['permeability'][str(numbers[i])]
+    fin['U']=df['darcy_velocity.x'][str(numbers[i])]
+    fin['W']=df['darcy_velocity.z'][str(numbers[i])]
     fin['sat']=df['saturation_liquid'][str(numbers[i])]
     fin['time']=i
     temp['h']=df['pressure'][str(numbers[i])]
@@ -42,23 +43,18 @@ for i in range(len(numbers)):
 #Assign homogeneous resistivity from field data
 df=out.copy()
 df['cond']=0
-df['cond'][df["porosity"]>=0.4]=1/40# 40 original used
-df['cond'][df["porosity"]<0.4]=1/800
-df1=df.iloc[:,[0,1,7,3,4,2,6,8,5]]
-df1.columns = ['X', 'Z', 't', 'U','W', 'Por','sat','cond','Perm']
+df['cond'][df["porosity"]>=0.1]=1/40# 40 original used
+df['cond'][df["porosity"]<0.1]=1/800
+df1=df.iloc[:,[0,1,6,3,4,2,5,7]]
+df1.columns = ['X', 'Z', 't', 'U','W', 'Por','sat','cond']
+df1["Perm"]=0
+df1['Perm'][df1["Por"]>=0.1]=param[0]# 40 original used
+df1['Perm'][df1["Por"]<0.1]=1.58e-12
 df1['m']=1.5
 df1['Qv']=0
 #Calculating Qv from REV method
-e0 = 1.602e-19   # Elementary charge (C)   
-C0=200*1.3e-5 # fluid conc. from 0.02 S/m using eqn from Griffin and Jurinak
-E=80.1*8.854e-12 # F/m
-kB=1.381e-23
-T=273.15+20
-tau=df1["Por"]**(1-df1['m'])
-phi=df1["Por"]
-k=df1['Perm']
-zeta=-0.00643+0.02085*np.log10(C0)
-df1['Qv']=param[4]*(E*(-zeta-zeta**3*(((e0) / ( kB * T))**2)/54)*(1 / tau**2) * (phi / k))/df1['sat'] #Soldi et al, 2019
+df1['Qv']=param[4]/df1['sat']
 df1.to_csv('data_set_edit.txt',index=False)
 with zipfile.ZipFile('data_set_edit.zip', 'w',zipfile.ZIP_DEFLATED) as myzip:
     myzip.write('data_set_edit.txt')
+
