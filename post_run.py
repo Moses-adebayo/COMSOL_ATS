984b076c8f7ae8b38342b4d53669e0e9e0ae1e87
Moses Adebayo (Student) <moses_adebayo@mines.edu>
2025-10-16 08:10:43 -0600

    starting new ensemble inversion

--- post_run.py
+++ post_run.py
@@ -23,7 +23,7 @@ matches = re.findall(pattern, content)
 
 # Convert matches to integers
 numbers = [int(match) for match in matches]
-loc=pd.read_csv('../data/mesh_cnt3.csv').iloc[:,[0,2]]
+loc=pd.read_csv('../data/mesh_final_step11.csv').iloc[:,[0,2]]
 for i in range(len(numbers)):
     fin=loc.copy()
     temp=loc.copy()
@@ -48,12 +48,13 @@ df['cond'][df["porosity"]<0.1]=1/800
 df1=df.iloc[:,[0,1,6,3,4,2,5,7]]
 df1.columns = ['X', 'Z', 't', 'U','W', 'Por','sat','cond']
 df1["Perm"]=0
-df1['Perm'][df1["Por"]>=0.1]=param[0]# 40 original used
-df1['Perm'][df1["Por"]<0.1]=1.58e-12
+df1['Perm'][df1["Por"]>=0.1]=param[1]# 40 original used
+df1['Perm'][df1["Por"]<0.1]=param[0]
 df1['m']=1.5
 df1['Qv']=0
 #Calculating Qv from REV method
-df1['Qv']=param[4]/df1['sat']
+df1['Qv']=param[2]/df1['sat']
+df1=df1[df1.t<244]
 df1.to_csv('data_set_edit.txt',index=False)
 with zipfile.ZipFile('data_set_edit.zip', 'w',zipfile.ZIP_DEFLATED) as myzip:
     myzip.write('data_set_edit.txt')
